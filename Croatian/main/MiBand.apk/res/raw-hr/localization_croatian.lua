fedf2612b081b10deaffd82f25927e5004778f30
MASVA <masva10@gmail.com>
2014-09-02 23:23:52 +0200

    Mi Band 01/09

--- Croatian/main/MiBand.apk/res/raw-hr/localization_croatian.lua
+++ Croatian/main/MiBand.apk/res/raw-hr/localization_croatian.lua
@@ -22,9 +22,9 @@ localization_Croatian_table = {
 
     welcome_use="Pozdrav, Ja sam tvoj Mi Band.",
     last_week_walked_format='Prošli tjedan, prohodali te %d koraka.',
-    last_week_walked_info_format='Prohodano %d km, potrošeno %d kcal.',
+    last_week_walked_info_format='Prohodano %d km, potrošeno %d kal.',
     last_month_walked_format='Prošli mjesec, prohodali ste %d koraka.',
-    last_month_walked_info_format='Prohodano %d km, potrošeno %d kcal.',
+    last_month_walked_info_format='Prohodano %d km, potrošeno %d kal.',
 
     get_over_format=", O %d grama ili masti.",
     click_to_get_help="Saznajte što sve možete s Mi Band.",
@@ -49,11 +49,11 @@ localization_Croatian_table = {
     activie_run_format_0="Trčano %s. Dobro je!",
     activie_run_format_1="Trčano %s. Ide vam fantastično!",
     activie_run_format_2="Trčano %s, Nevjerojatno ste izdržljivi!",
-    activie_run_consumed="Potrošeno %d kcal. %s",
+    activie_run_consumed="Potrošeno %d kal. %s",
 
     get_distance_format=", ukupno od %d m.",
     activity_walk_format="%s Prohodano %d koraka %s",
-    activity_walk_consumed_format="Potrošeno %d kcal %s",
+    activity_walk_consumed_format="Potrošeno %d kal %s",
 
     activity_activity_format="%s Napravljeno %s %s",
     last_night_sleeped_good_format="Prošle noći ste spavali %d sati %d min. Sad ste stvarno naspavani!",
@@ -65,4 +65,7 @@ localization_Croatian_table = {
 
     cannot_find_bracelet="Ne možete pronaći svoj Mi Band?",
     cannot_find_bracelet_info="Uvjerite se da vaš MiBand ima energije i nalazi se u blizini vašeg telefona.",
+    challenge_to_get="%s dana od postizanja novog rekorda!",
+    record_reach_max="Postigli ste svoj najveći novi rekord!",
+    new_record_born="Novi rekord!",
  }
